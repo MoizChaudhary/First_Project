b6221d7b6e1e588b50e06342e97fa4f3ec397273
MoizChaudhary <moizchaudhary1234@gmail.com>
2024-08-17 15:24:52 -0700

    data fetched

--- App.tsx
+++ App.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navigator from './src/screens/navigation/stackNavigation';
-import { SafeAreaView } from 'react-native';
-import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+
+import {SafeAreaView} from 'react-native';
 
 const App = () => {
-  
-  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userDoc = await firestore()
+          .collection('user')
+          .doc('RfV1DvqE9VqCkPcZHayo')
+          .get();
+        console.log('Document data:', userDoc.data());
+      } catch (error) {
+        console.error('Error fetching document:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <SafeAreaView style={{ backgroundColor: 'white', flex: 1 }}>
+    <SafeAreaView style={{backgroundColor: 'white', flex: 1}}>
       <Navigator />
     </SafeAreaView>
   );
